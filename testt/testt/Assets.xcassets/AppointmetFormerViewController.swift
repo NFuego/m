9a6511439d26adce02341f33b89d6b4428081dc1
tomas <zcon.tomas@gmail.com>
2017-03-10 14:58:41 +0800

    add ImgRow

--- testt/testt/Assets.xcassets/AppointmetFormerViewController.swift
+++ testt/testt/Assets.xcassets/AppointmetFormerViewController.swift
@@ -1,18 +1,11 @@
-//  AppointmetFormerViewController.swift
-//  Project: testt
-//
-//  Module: AppointmetFormer
-//
-//  By zcon 2017/3/9
-//  test 2017年
-//
-
 // MARK: Imports
 
 import UIKit
 
 import Eureka
 import SwiftyVIPER
+import Kingfisher
+import SnapKit
 
 // MARK: Protocols
 
@@ -34,6 +27,7 @@ class AppointmetFormerViewController: FormViewController {
 	let presenter: AppointmetFormerViewPresenterProtocol
 
 	// MARK: Variables
+    let lbColor = UIColor.black
 
 	// MARK: Inits
 	init(presenter: AppointmetFormerViewPresenterProtocol) {
@@ -55,6 +49,7 @@ class AppointmetFormerViewController: FormViewController {
     }
 }
 
+
 // MARK: - AppointmetFormer Presenter to View Protocol
 extension AppointmetFormerViewController: AppointmetFormerPresenterViewProtocol {
 	func set(title: String?) {
@@ -66,10 +61,50 @@ extension AppointmetFormerViewController: AppointmetFormerPresenterViewProtocol 
 extension AppointmetFormerViewController {
     func setupForm(){
         let header = "Appoint"
-        
+
+        /*
+         loaded :
+
+
+         icon
+         name
+         class
+         weight
+         age
+
+         
+         descript:
+         owner name
+         email
+         phone
+         address
+ */
+//         icon
+//         name
+//         class
+//         weight
+//         age
+//
+//
+//         descript:
+//         owner name
+//         email 
+//         phone
+//         address
+
             form  +++ Section(header: header, footer: "")          //bleConnMsg)
+                <<< ImgRow() { (i:ImgRow) -> Void in
+                    i.value = Picture(url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVKqyeR1MompJT0wpeE_24HEIF-DlXAkyz69a_qTYxhxf_GVXneA")
+                    
+                }
+                <<< LabelRow() { (l:LabelRow) -> Void in
+                    l.title = "hihi"
+                    
+                } .onCellSelection({ (cell, label) in
+                    
+                })
                 <<< NameRow() { (row:NameRow) -> Void in
-                    row.title = "name" //NSLocalizedString("blevc-scan", comment: "")
+                    row.title = "petname" //NSLocalizedString("blevc-scan", comment: "")
                     row.cell.tintColor = .green
                     row.cell.textLabel?.text =  "fwfjwoiejfoiwe"
                 } .onCellSelection({ (cell, row) in
@@ -90,12 +125,70 @@ extension AppointmetFormerViewController {
                         
                     ).onChange({ (row:TextRow) in
                 })
-                <<< LabelRow() { (r:LabelRow) -> Void in
-                    r.title = "hihi"
-                    
-                } .onCellSelection({ (cell, label) in
-                    
-                })
 //                +++ bleListSec
     } // fin setupForm
+}
+
+// ===================================================================================  new row & cell
+
+struct Picture: Equatable {
+    var url: String?
+}
+//
+func ==(lhs: Picture, rhs: Picture) -> Bool {
+    return lhs.url == rhs.url
+}
+
+
+
+final class ImgCell : Cell<Picture>,CellType {
+
+    let img:UIImageView = UIImageView()
+    let imgSize = 100
+    let processor = RoundCornerImageProcessor(cornerRadius: 50)
+
+    required init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+
+        self.contentView.addSubview(img)
+        self.img.snp.makeConstraints { (make) in
+            make.center.equalToSuperview()
+            make.size.equalTo(imgSize)
+        }
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+    }
+
+    override func setup() {
+        super.setup()
+        selectionStyle = .none
+        height = { return 120 }
+    }
+
+
+    override func update() {
+        guard let pic = row.value else { return }
+        if let url = pic.url {
+            self.img.kf.setImage(with: URL(string:url), options: [.transition(.fade(0.2)), .processor(processor)])
+            self.img.kf.indicatorType = .activity
+        }
+    }
+}
+
+final class ImgRow : Row<ImgCell>, RowType {
+    required init(tag: String?) {
+        super.init(tag: tag)
+        cellProvider = CellProvider<ImgCell>()
+    }
 }
