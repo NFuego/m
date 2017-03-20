277ed6f09f780f9b517358b282df24ac813d3496
tomas <zcon.tomas@gmail.com>
2017-03-20 17:29:34 +0800

    Have fun

--- testt/testt/ViewController.swift
+++ testt/testt/ViewController.swift
@@ -5,26 +5,53 @@ import SwiftyJSON
 import SnapKit
 
 class ViewController: UIViewController {
+
+    let lbCommitMsg = UILabel()
+    var imw:UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-//        self.view.backgroundColor = .blue
-//        self.edgesForExtendedLayout = []
+        self.view.backgroundColor = .white
+        self.edgesForExtendedLayout = []
+        self.navigationController?.navigationBar.barStyle = .blackOpaque
+        self.navigationController?.navigationBar.isTranslucent = false
+        self.navigationController?.navigationBar.tintColor = .cyan
+        self.navigationController?.navigationBar.setBackgroundImage(UIImage(named:"navBar"), for: .default)
+
 //        self.extendedLayoutIncludesOpaqueBars = []
-        self.navigationController?.navigationBar.isTranslucent = true
         let title = NSLocalizedString("origami", comment: "")
-        self.title = title
-        setupBg()
+        self.title = "Origami Time ver. 1.0"
+        self.setupFramePhoto()
+//        setupBg()
         commitAsMessage()
+        authorWords()
     }
 
     func setupBg(){
         let imw = UIImageView(image: UIImage(named:"origami_time_bg"))
         self.view.addSubview(imw)
         imw.snp.makeConstraints { (make) in
-//            make.equalToSuperview()
-//            make.equalToSuperview()
             make.size.equalToSuperview()
         }
+    }
+
+    func setupFramePhoto(){
+        imw = UIImageView(image: UIImage(named:"origami_time_bg"))
+        self.view.addSubview(imw)
+//        let degrees:CGFloat = 10
+//        imw.layer.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+
+        imw.snp.makeConstraints { (make) in
+//            make.equalToSuperview()
+//            make.equalToSuperview()
+
+            make.size.width.equalTo(472 * 0.6)
+            make.size.height.equalTo(709 * 0.6)
+            make.centerX.equalToSuperview()
+            make.bottom.equalToSuperview().offset(-10)
+        }
+//        imw.transform = CGAffineTransform(rotationAngle: degrees * CGFloat(M_PI/180) )
+
     }
 
 
@@ -42,18 +69,17 @@ class ViewController: UIViewController {
                 msg = (json.array?.first?.dictionary?["commit"]!["message"].stringValue)!
                 print(msg)
             }// Do any additional setup after loading the view, typically from a nib.
-            let t = UILabel()
-            t.textAlignment = .center
+            self.lbCommitMsg.textAlignment = .center
 
-            self.view.addSubview(t)
+            self.view.addSubview(self.lbCommitMsg)
             let attrStr = try! NSAttributedString(
                 data: msg.data(using: String.Encoding.unicode, allowLossyConversion: true)!,
                 options: [ NSDocumentTypeDocumentAttribute: NSHTMLTextDocumentType],
                 documentAttributes: nil)
-            t.attributedText = attrStr
-            t.snp.makeConstraints({ (make) in
+            self.lbCommitMsg.attributedText = attrStr
+            self.lbCommitMsg.snp.makeConstraints({ (make) in
                 make.centerX.equalToSuperview()
-                make.top.equalToSuperview().offset(75)
+                make.top.equalToSuperview().offset(10)
                 make.width.equalToSuperview()
                 make.height.equalTo(70)
             })
@@ -62,8 +88,46 @@ class ViewController: UIViewController {
     }
 
     func authorWords(){
+        let t = UITextView()
+//        let t = UILabel()
+        t.textColor = .black
+//        t.textAlignment = .center
+//        t.numberOfLines = 5
+        t.textAlignment = .center
+        t.text = "I think Origami is a very elegant art, \n You could concentrate when you are folding., \ni hope Origami Time could bring you\n a wonderful time.ii"
+        // blur
+//        let be = UIBlurEffect(style: .extraLight)
+//        let v = UIVisualEffectView(effect: be)
+//
+//        v.backgroundColor = UIColor.white.withAlphaComponent(0.2)
+//        v.clipsToBounds = true
+//        v.layer.borderColor = UIColor.white.withAlphaComponent(0.2).cgColor
+//        v.layer.borderWidth = 1.0
+//        v.layer.cornerRadius = 6.0
         
+//        t.textColor = UIColor.black.withAlphaComponent(0.3)
+//        t.sizeToFit()
+//        v.addSubview(t)
+        self.view.addSubview(t)
+        t.snp.makeConstraints { (make) in
+            make.width.equalToSuperview()
+            make.height.equalTo(90)
+//            make.bottom.equalToSuperview().offset(-50)
+//            make.top.equalTo(t.snp.bottom).offset(100)
+            make.bottom.equalTo(imw.snp.top).offset(-30)
+            make.centerX.equalToSuperview()
+        }
+        
+//
+//        // label
+//        UILabel *label = [[UILabel alloc] init];
+//        label.textColor = [[UIColor blackColor] colorWithAlphaComponent:0.7f];
+//        [label sizeToFit];
+//        
+//        // add the label to effect view
+//        [blurView.contentView addSubview:label];
     }
+    
 
 
     override func didReceiveMemoryWarning() {
